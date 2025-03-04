abd39dfeaa7a7352b7d6588f439b55ae77061706
Marina Kinalone Simonnet <marina.simonnet@trustly.com>
2025-03-04 17:08:31 +0000

    add window frame and button functionality

--- experiences/fenêtres/index.tsx
+++ experiences/fenêtres/index.tsx
@@ -1,6 +1,7 @@
 import { getDownloadURL, listAll, ref } from 'firebase/storage'
 import React, { useEffect, useState } from 'react'
 import Window from './Window'
+import styles from './styles/Fenetres.module.scss'
 import { storage } from './utils/firebase'
 
 const FIREBASE_FOLDER = 'fenetres'
@@ -20,7 +21,8 @@ const getWindowInfo = (itemName: string): IWindowInfo => {
   const [yearMonth, cityCountry] = itemName.split(' - ')
   const [year, month] = yearMonth.split(' ')
   const [city, countryWithExtension] = cityCountry.split(', ')
-  const country = countryWithExtension.split('.')[0]
+  const countryNoExtension = countryWithExtension.split('.')[0]
+  const country = countryNoExtension.split(' (')[0]
 
   return {
     year,
@@ -30,11 +32,14 @@ const getWindowInfo = (itemName: string): IWindowInfo => {
   }
 }
 
+// TODO add user interaction to trigger autoplay
+
 const Fenêtres = () => {
   const [loading, setLoading] = useState<boolean>(true)
   const [error, setError] = useState<boolean>(false)
   const [windows, setWindows] = useState<IWindow[]>([])
   const [selectedWindow, setSelectedWindow] = useState<IWindow | null>(null)
+  const [previousIndex, setPreviousIndex] = useState<number | null>(null)
 
   const fetchWindowList = async () => {
     const listRef = ref(storage, FIREBASE_FOLDER)
@@ -54,23 +59,33 @@ const Fenêtres = () => {
     }
   }
 
+  const handleOpenWindow = () => {
+    if (windows.length > 0) {
+      let randomIndex
+      do {
+        randomIndex = Math.floor(Math.random() * windows.length)
+      } while (randomIndex === previousIndex)
+      setSelectedWindow(windows[randomIndex])
+      setPreviousIndex(randomIndex)
+    }
+  }
+
   useEffect(() => {
     fetchWindowList()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   useEffect(() => {
-    if (windows.length > 0) {
-      const randomIndex = Math.floor(Math.random() * windows.length)
-      setSelectedWindow(windows[randomIndex])
-    }
+    handleOpenWindow()
   }, [windows])
 
   return (
-    <div>
-      {loading && <div>Loading...</div>}
-      {!loading && !error && selectedWindow && <Window {...selectedWindow} />}
-      {error && <div>Error fetching windows</div>}
+    <div className={styles.main__container}>
+      {loading && <p>Loading...</p>}
+      {!loading && !error && selectedWindow && (
+        <Window {...selectedWindow} handleOpenWindow={handleOpenWindow} />
+      )}
+      {error && <p>Error fetching windows</p>}
     </div>
   )
 }
